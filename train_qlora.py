d5791692308ee95388f5290aa9667051032543fb
valkryhx <283306449@qq.com>
2023-07-19 00:25:21 +0800

    Update train_qlora.py

--- train_qlora.py
+++ train_qlora.py
@@ -147,8 +147,8 @@ def init_components(args, training_args):
             bnb_4bit_compute_dtype=torch.float16,
             bnb_4bit_use_double_quant=True,
             bnb_4bit_quant_type="nf4",
-            llm_int8_threshold=6.0,
-            llm_int8_has_fp16_weight=False,
+            #llm_int8_threshold=6.0,
+            #llm_int8_has_fp16_weight=False,
         ),
     )
     # 加载tokenzier
