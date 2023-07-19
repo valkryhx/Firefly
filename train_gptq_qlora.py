23fba1744d10366322e38f8cb2ff497ae2e65726
valkryhx <283306449@qq.com>
2023-07-19 21:08:12 +0800

    Create train_gptq_qlora.py

---
+++ train_gptq_qlora.py
@@ -0,0 +1,332 @@
+from transformers import AutoTokenizer, BitsAndBytesConfig
+#from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
+from peft import (
+    TaskType,
+    LoraConfig,
+    AdaLoraConfig ,  #  提出自2020年 感觉和lora区别不大 而且和qlora有冲突 这里代码没有用到 
+                     #例子https://www.zhihu.com/question/596950521/answer/3109759716
+    get_peft_model,
+    set_peft_model_state_dict,
+    prepare_model_for_kbit_training
+)
+from transformers import (
+    set_seed,
+    HfArgumentParser,
+    TrainingArguments,
+    AutoModelForCausalLM
+)
+import argparse
+from loguru import logger
+import os
+from os.path import join
+import torch
+import bitsandbytes as bnb
+from collections import defaultdict
+
+from component.collator import SFTDataCollator
+from component.dataset import SFTDataset
+from component.argument import QLoRAArguments
+from component.trainer import LoRATrainer
+from component.loss import TargetLMLoss
+
+from transformers.deepspeed import HfDeepSpeedConfig
+import deepspeed
+from transformers.deepspeed import HfDeepSpeedConfig
+import json
+from auto_gptq import AutoGPTQForCausalLM
+from auto_gptq.utils.peft_utils import get_gptq_peft_model, GPTQLoraConfig
+from auto_gptq import AutoGPTQForCausalLM
+from auto_gptq.nn_modules.qlinear import GeneralQuantLinear
+
+
+def verify_model_dtype(model):
+    """
+    查看模型种各种类型的参数的情况
+    """
+    dtype2param_num = defaultdict(int)  # 每种数据类型的参数量
+    dtype2param_name = defaultdict(list)  # 每种数据类型的参数名称
+    dtype2trainable_param_num = defaultdict(int)  # 每种数据类型参与训练的参数量
+    dtype2trainable_param_name = defaultdict(list)  # 每种数据类型参与训练的参数名称
+    for name, p in model.named_parameters():
+        dtype = p.dtype
+        dtype2param_num[dtype] += p.numel()
+        dtype2param_name[dtype].append(name)
+        if p.requires_grad:
+            dtype2trainable_param_num[dtype] += p.numel()
+            dtype2trainable_param_name[dtype].append(name)
+    # 统计全部参数中，各种类型参数分布
+    total = 0
+    print('verify all params of the model')
+    for k, v in dtype2param_num.items():
+        total += v
+    for k, v in dtype2param_num.items():
+        print(k, v, v / total)
+    for k, v in dtype2trainable_param_name.items():
+        print(k, v)
+
+    print()
+    # 统计可训练参数中，各种类型参数分布
+    print('verify trainable params the model')
+    total_trainable = 0
+    for k, v in dtype2trainable_param_num.items():
+        total_trainable += v
+    for k, v in dtype2trainable_param_num.items():
+        print(k, v, v / total_trainable)
+    for k, v in dtype2trainable_param_num.items():
+        print(k, v)
+
+
+def find_all_linear_names(model):
+    """
+    找出所有全连接层，为所有全连接添加adapter
+    """
+    #cls = bnb.nn.Linear4bit
+    cls = GeneralQuantLinear if not(args.full_finetune) else torch.nn.Linear
+    lora_module_names = set()
+    for name, module in model.named_modules():
+        if isinstance(module, cls):
+            names = name.split('.')
+            lora_module_names.add(names[0] if len(names) == 1 else names[-1])
+
+    if 'lm_head' in lora_module_names:  # needed for 16-bit
+        lora_module_names.remove('lm_head')
+    if  'output_layer' in lora_module_names:  # # needed for 16-bit ,chatglm2-6b use output_layer instead of lm_head
+        lora_module_names.remove('output_layer')
+    return list(lora_module_names)
+
+
+def setup_everything():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--train_args_file", type=str, default='train_args/qlora/baichuan-sft-qlora.json', help="")
+    parser.add_argument("--deepspeed", type=str, default="train_args/qlora/ds_zero2_config.json")
+    # local_rank 要加入argument ，因为使用deepspeed会传入这个参数 不加的话会报错 unrecognized argument
+    # 参考我写的chatGLM-6B-QLoRA/train_qlora_deepspeed_zero.py
+    parser.add_argument("--local_rank", type=int, default=0)
+    args = parser.parse_args()
+    train_args_file = args.train_args_file
+    
+    #here we get deepspeed config  before the args changed by the line#92 args, training_args = parser.parse_json_file(json_file=train_args_file)
+    with open(args.deepspeed,'r',encoding='utf-8') as fr:   # 这里就是向TrainingArgs中添加deepseed字段
+        training_args_deepspeed = json.load(fr)  # set trainingArgs中deepspeed=ds_config
+        
+    # 读取训练的参数配置
+    parser = HfArgumentParser((QLoRAArguments, TrainingArguments))
+    # 解析得到自定义参数，以及自带参数
+    args, training_args = parser.parse_json_file(json_file=train_args_file)
+    training_args.deepspeed = training_args_deepspeed
+    # 创建输出目录
+    if not os.path.exists(training_args.output_dir):
+        os.makedirs(training_args.output_dir,exist_ok=True)
+    # logger.add(join(training_args.output_dir, 'train.log'))
+    # logger.info("train_args:{}".format(training_args))
+    # 设置随机种子
+    set_seed(training_args.seed)
+    return args, training_args
+
+
+def init_components(args, training_args):
+    """
+    初始化各个组件
+    """
+    logger.info('Initializing components...')
+
+    """
+    # 下面的设置至关重要，否则无法多卡训练
+    world_size = int(os.environ.get("WORLD_SIZE", 1))
+    ddp = world_size != 1
+    training_args.ddp_find_unused_parameters = False
+    device_map = "auto"
+    # if we are in a distributed setting, we need to set the device map and max memory per device
+    if os.environ.get('LOCAL_RANK') is not None:
+        local_rank = int(os.environ.get('LOCAL_RANK', '0'))
+        device_map = {'': local_rank}
+   """
+    ds_config ={
+    "fp16": {
+        "enabled": True,
+        "loss_scale": 0,
+        "loss_scale_window": 1000,
+        "initial_scale_power": 16,
+        "hysteresis": 2,
+        "min_loss_scale": 1
+    },
+    "bf16": {
+        "enabled": False
+    },
+    "optimizer": {
+        "type": "AdamW",
+        "params": {
+            "lr": "auto",
+            "weight_decay": "auto",
+            "torch_adam": True,
+            "adam_w_mode": True
+        }
+    },
+    "scheduler": {
+        "type": "WarmupDecayLR",
+        "params": {
+            "warmup_min_lr": "auto",
+            "warmup_max_lr": "auto",
+            "warmup_num_steps": "auto",
+            "total_num_steps": "auto"
+        }
+    },
+
+    "zero_optimization": {
+        "stage": 3,
+        "offload_optimizer": {
+            "device": "cpu",
+            "pin_memory": False
+        },
+        "offload_param": {
+            "device": "none",
+            "pin_memory": False
+        },
+        "overlap_comm": True,
+        "contiguous_gradients": True,
+        "sub_group_size": 1e6,
+        "reduce_bucket_size": "auto",
+        "stage3_prefetch_bucket_size": "auto",
+        "stage3_param_persistence_threshold": "auto",
+        "stage3_max_live_parameters": 1e6,
+        "stage3_max_reuse_distance": 1e6,
+        "stage3_gather_16bit_weights_on_model_save": True
+    },    
+    "train_batch_size": 8 ,  
+    "train_micro_batch_size_per_gpu":4
+}
+    dschf = HfDeepSpeedConfig(ds_config)
+    # 加载模型
+    # model = AutoModelForCausalLM.from_pretrained(
+    #     args.model_name_or_path,
+    #     #device_map=device_map,
+    #     #device_map="auto",  # DeepSpeed Zero-3 is not compatible with `low_cpu_mem_usage=True` or with passing a `device_map`.
+    #     load_in_4bit=True,
+    #     torch_dtype=torch.float16,
+    #     trust_remote_code=True,
+    #     empty_init=False, 
+    #     quantization_config=BitsAndBytesConfig(
+    #         load_in_4bit=True,
+    #         bnb_4bit_compute_dtype=torch.float16,
+    #         bnb_4bit_use_double_quant=True,
+    #         bnb_4bit_quant_type="nf4",
+    #         #llm_int8_threshold=6.0,
+    #         #llm_int8_has_fp16_weight=False,
+    #     ),
+    # )
+
+    model = AutoGPTQForCausalLM.from_quantized(
+        args.model_name_or_path,
+        device_map='auto',
+        max_memory= 12000 ,  # MB ,max_memory,
+        trust_remote_code=True,
+        inject_fused_attention = True,
+        inject_fused_mlp = False,
+        use_triton=True,
+        warmup_triton=False,
+        trainable=True
+    )
+    model.model.quantize_config = model.quantize_config
+    model.train()
+    setattr(model, 'model_parallel', True)
+    setattr(model, 'is_parallelizable', True)
+
+    # fp32 is really important during training!
+    model.config.torch_dtype=(torch.float32 if args.fp16 else (torch.bfloat16 if args.bf16 else torch.float32))
+  
+    # 加载tokenzier
+    tokenizer = AutoTokenizer.from_pretrained(
+        args.model_name_or_path,
+        trust_remote_code=True,
+        # llama不支持fast
+        use_fast=False if model.config.model_type == 'llama' else True
+    )
+    # 部分tokenizer没有pad_token_id
+    if tokenizer.pad_token_id is None:
+        tokenizer.pad_token_id = tokenizer.unk_token_id
+    # 如果两者相同，模型训练时不会计算eos_token_id的loss
+    if tokenizer.pad_token_id == tokenizer.eos_token_id:
+        raise Exception('pad_token_id should not be equal to eos_token_id')
+
+    # casts all the non int8 modules to full precision (fp32) for stability
+    model.enable_input_require_grads()
+    model = prepare_model_for_kbit_training(model, use_gradient_checkpointing=training_args.gradient_checkpointing)
+    print(f'memory footprint of model: {model.get_memory_footprint()/(1024*1024*1024)} GB')
+    # 找到所有需要插入adapter的全连接层
+    target_modules = find_all_linear_names(model)
+    # 初始化lora配置
+    # config = LoraConfig(
+    #     r=args.lora_rank,
+    #     lora_alpha=args.lora_alpha,
+    #     target_modules=target_modules,
+    #     lora_dropout=args.lora_dropout,
+    #     bias="none",
+    #     task_type="CAUSAL_LM",
+    # )
+     config = GPTQLoraConfig(
+        r=args.lora_rank,
+        lora_alpha=args.lora_alpha,
+        target_modules=target_modules,
+        lora_dropout=args.lora_dropout,
+        bias="none",
+        task_type="CAUSAL_LM",
+    )
+    #model = get_peft_model(model, config)
+    model = get_gptq_peft_model(model, config, auto_find_all_linears=False, train_mode=True)
+    model.print_trainable_parameters()
+    #model.config.torch_dtype = torch.float16 #torch.float32
+
+    for name, module in model.named_modules():
+        if isinstance(module, LoraLayer):
+            if args.bf16:
+                module = module.to(torch.bfloat16)
+        if 'norm' in name:
+            module = module.to(torch.float32)
+        if 'lm_head' in name or 'embed_tokens' in name:
+            if hasattr(module, 'weight'):
+                if args.bf16 and module.weight.dtype == torch.float32:
+                    module = module.to(torch.bfloat16)
+
+    # 查看模型种各种类型的参数的情况
+    verify_model_dtype(model)
+
+    # 初始化损失函数
+    loss_func = TargetLMLoss(ignore_index=tokenizer.pad_token_id)
+
+    # 加载训练集
+    train_dataset = SFTDataset(args.train_file, tokenizer, args.max_seq_length)
+    data_collator = SFTDataCollator(tokenizer, args.max_seq_length)
+
+    print(training_args)
+    # 初始化Trainer
+    trainer = LoRATrainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        tokenizer=tokenizer,
+        data_collator=data_collator,
+        compute_loss=loss_func
+    )
+    return trainer
+
+
+def main():
+    # 进行一些配置和检查
+    args, training_args = setup_everything()
+    # 加载各种组件
+    trainer = init_components(args, training_args)
+    # 开始训练
+    logger.info("*** starting training ***")
+    train_result = trainer.train()
+    # 保存最好的checkpoint
+    final_save_path = join(training_args.output_dir, 'final')
+    trainer.save_model(final_save_path)  # Saves the tokenizer too
+    # 保存训练指标
+    metrics = train_result.metrics
+    trainer.log_metrics("train", metrics)
+    trainer.save_metrics("train", metrics)
+    trainer.save_state()
+
+
+if __name__ == "__main__":
+    main()
