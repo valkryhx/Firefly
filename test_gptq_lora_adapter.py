9b8ba796422f451b4f4c36eb59369c084880437c
valkryhx <283306449@qq.com>
2023-07-21 17:06:17 +0800

    Update test_gptq_lora_adapter.py

--- test_gptq_lora_adapter.py
+++ test_gptq_lora_adapter.py
@@ -51,7 +51,7 @@ def main():
     args = parser.parse_args()
     # model_name = 'YeungNLP/firefly-baichuan-7b-qlora-sft-merge'
     #model_name =  args.base_model_name_or_path  #'TheBloke/baichuan-7B-GPTQ'
-    model_name = 'fireballoon/baichuan-vicuna-chinese-7b-gptq'
+    model_name = "fireballoon/baichuan-vicuna-chinese-7b-gptq"
     logger.info(f"args.use_safetensors= {args.use_safetensors}")
     max_new_tokens = 500
     top_p = 0.9
