2a50923a7f593e4b7180c5500b0b03e2ff097b23
valkryhx <283306449@qq.com>
2023-07-21 17:11:57 +0800

    Update test_gptq_lora_adapter.py

--- test_gptq_lora_adapter.py
+++ test_gptq_lora_adapter.py
@@ -70,7 +70,7 @@ def main():
         use_triton=True,
         warmup_triton=False,
         trainable=False,
-        use_safetensors = args.use_safetensors ,
+        use_safetensors = False,
     )
     tokenizer = AutoTokenizer.from_pretrained(
         model_name,
